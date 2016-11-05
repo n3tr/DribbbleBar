652faf505fc94b85ffb9824b26a5cb26f3e8b631
Jirat K <saakyz@gmail.com>
2016-11-05 23:13:13 +0700

    Initial Commit

---
+++ DribbbleBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DribbbleBar
+//
+//  Created by Jirat Ki. on 11/5/2559 BE.
+//  Copyright © 2559 Jirat Ki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DribbbleBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DribbbleBar
+//
+//  Created by Jirat Ki. on 11/5/2559 BE.
+//  Copyright © 2559 Jirat Ki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
